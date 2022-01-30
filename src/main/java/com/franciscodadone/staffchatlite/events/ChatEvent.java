31642245dd3973852f15b29cd3a2b5cd0184ccf7
FranciscoDadone <dadonefran@gmail.com>
2022-01-30 10:01:06 -0300

    Send sc when using # at start

--- src/main/java/com/franciscodadone/staffchatlite/events/ChatEvent.java
+++ src/main/java/com/franciscodadone/staffchatlite/events/ChatEvent.java
@@ -1,6 +1,7 @@
 package com.franciscodadone.staffchatlite.events;
 
 import com.franciscodadone.staffchatlite.chat.ChatManager;
+import com.franciscodadone.staffchatlite.permissions.PermissionTable;
 import com.franciscodadone.staffchatlite.storage.Global;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -12,10 +13,20 @@ public class ChatEvent implements Listener {
     @EventHandler
     public void onChat(AsyncPlayerChatEvent e) {
         Player player = e.getPlayer();
+        if(player.hasPermission(PermissionTable.chat)) {
+            // Send message when sc toggle
+            if(Global.playersToggledStaffChat.contains(player)) {
+                e.setCancelled(true);
+                ChatManager.sendStaffChatMessage(player, e.getMessage());
+            }
 
-        if(Global.playersToggledStaffChat.contains(player)) {
-            e.setCancelled(true);
-            ChatManager.sendStaffChatMessage(player, e.getMessage());
+            // Send message when #
+            if(e.getMessage().startsWith(Global.plugin.getConfig().getString("sc-send-alias"))) {
+                e.setCancelled(true);
+                String message = e.getMessage().replaceFirst(Global.plugin.getConfig().getString("sc-send-alias"), "");
+                ChatManager.sendStaffChatMessage(player, message);
+            }
+
         }
 
     }
